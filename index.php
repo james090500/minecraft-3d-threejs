68db50f5b3f9433819b147d57b6778fcacc86755
James Harrison <james@fasttortoise.co.uk>
2019-08-22 17:01:23 +0100

    Working on adding textures to 3d model

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ThreeJS Test</title>
+  </head>
+  <body>
+    <script src="/js/three.min.js"></script>
+    <script src="/js/OrbitControls.js"></script>
+    <script src="/js/capes.js"></script>
+  </body>
+</html>
--- js/capes.js
+++ js/capes.js
@@ -1,21 +1,41 @@
 /* eslint no-undef: 0 */
-var renderer = new THREE.WebGLRenderer({ alpha: true })
-renderer.setSize(300, 400)
-document.body.appendChild(renderer.domElement)
+function init() {
+  var renderer = new THREE.WebGLRenderer() //{ alpha: true }
+  renderer.setSize(300, 400)
+  document.body.appendChild(renderer.domElement)
 
-var scene = new THREE.Scene()
-var camera = new THREE.PerspectiveCamera(3, 300 / 400, 1, 10000)
-var controls = new THREE.OrbitControls(camera, renderer.domElement)
+  var scene = new THREE.Scene()
+  var camera = new THREE.PerspectiveCamera(3, 300 / 400, 1, 10000)
+  var controls = new THREE.OrbitControls(camera, renderer.domElement)
 
-var geometry = new THREE.BoxGeometry(1, 1, 1)
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-var cube = new THREE.Mesh(geometry, material)
+  camera.position.set(0, 20, 100)
+  controls.enableZoom = false
+  controls.update()
 
-scene.add(cube)
+  scene.add(createPlayerGroup('t'))
 
-camera.position.set(0, 20, 100)
-controls.enableZoom = false
-controls.update()
+  render()
+}
+
+function createPlayerGroup(uuid) {
+  let headGroup = new THREE.Object3D()
+  headGroup.position.x = 0
+  headGroup.position.y = 12
+  headGroup.position.z = 0
+  let box = new THREE.BoxGeometry(8, 8, 8, 8, 8, 8)
+  let headMesh = colorFaces(box, opaqueSkinCanvas, skinLayout[version]['head'][0])
+  headGroup.add(headMesh)
+  if (hasAlpha) {
+    box = new THREE.BoxGeometry(9, 9, 9, 8, 8, 8)
+    let hatMesh = colorFaces(box, transparentSkinCanvas, skinLayout[version]['head'][1])
+    hatMesh && headGroup.add(hatMesh)
+  }
+
+  let playerGroup = new THREE.Object3D()
+  playerGroup.add(headGroup)
+
+  return playerGroup
+}
 
 function render () {
   requestAnimationFrame(render)
@@ -23,4 +43,4 @@ function render () {
   renderer.render(scene, camera)
 }
 
-render()
+init()
